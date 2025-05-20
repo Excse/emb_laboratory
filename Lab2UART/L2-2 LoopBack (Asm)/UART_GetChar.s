69d68be7feac899352368c6e1492ae260a669731
timo <behrendtimo@yahoo.de>
2025-05-20 17:49:23 +0200

    Finished the second task

--- Lab2UART/L2-2 LoopBack (Asm)/UART_GetChar.s
+++ Lab2UART/L2-2 LoopBack (Asm)/UART_GetChar.s
@@ -3,19 +3,35 @@
 
     INCLUDE    LPC1768.inc
     EXPORT     UART_GetChar
+		
     ;Assumption R0 = UART_PortNum (0 or 2 for LandTiger EVB)
 
 UART_GetChar    PROC
-;LSR: RDR lesen
-;wenn gesetzt, RBR lesen
-    STMFD    SP!,{... , LR}
-    ;
-    ; PUT YOUR CODE HERE
-    ;
+    STMFD SP!, {R1-R3, LR}
+    CBZ   R0, UART_Port0   ; Check if UART Port 0 or Port 2 selected
+
+UART_Port2
+    LDR R1, =U2LSR         ; UART Port 2 selected
+    LDR R2, =U2RBR		   ; UART Port 2 RBR
+    B   Check_RDR
+
+UART_Port0
+    LDR R1, =U0LSR         ; UART Port 0 selected
+    LDR R2, =U0RBR		   ; UART Port 0 RBR
+	
+Check_RDR
+    LDRB R3, [R1]          ; Lade Inhalt des LSR
+	TST  R3, #0x0001       ; Prüfe Bit 0 (RDR - Receiver Data Ready)
+	BEQ  No_Data
+	
+Has_Data
+	LDRB R0, [R2]
+	B    UART_Rx_Done
+	
+No_Data
+	LDR R0, =0xFFFFFFFF    ; Kein Zeichen, somit -1 zurückgeben
 
 UART_Rx_Done
-    LDMFD    SP!,{..., PC}
-
-    ENDP        ; End Procedure
-
+    LDMFD    SP!, {R1-R3, PC}
+    ENDP
     END
--- Lab2UART/L2-2 LoopBack (Asm)/UART_LoopBack_ASM.s
+++ Lab2UART/L2-2 LoopBack (Asm)/UART_LoopBack_ASM.s
@@ -10,11 +10,31 @@
     EXPORT __main
     ENTRY
 __main    PROC
+	
+Configure_UART
+    LDR R0, =0x0002     ; UART_PortNum                
+    LDR R1, =0x000B     ; UxDL  = (UxDLM << 8) | (UxDLL)
+    LDR R2, =0xD303     ; (UxFDR (7:4 MULVAL  3:0 DIVADDVAL) <<8) | UxLCR (UART_Mode) ):
+                        ; DLAB 0  Set Break 0  Stick Parity 0  Even Parity Select 0  
+                        ; Parity Enable 0  Number of Stop Bits 0  Word Length Select 11
+    LDR R3, =0x0007     ; (UxIER << 8) | UxFCR)        
+	                    ; TX FIFO Reset 1 ; RX FIFO Reset 1 ; FIFO Enable 1
 
-    ;
-    ; PUT YOUR CODE HERE
-    ;
+	MOV R4, R0          ; Copy of PortNum (weil R0..R3 Scratch-Register sind, d.h. in einer gerufenen Funtion verändert werden können!)
+	BL  UART_init
 
-    ENDP    ; End Procedure
+Loopback
+    MOV R0, R4          ; Setze R0 wieder auf UART_PortNum.
+    BL  UART_GetChar    ; Lese ein Zeichen.
+	
+    LDR R1, =0xFFFFFFFF
+    CMP R0, R1          ; Teste ob in R0 der Wert 0xFFFFFFFF steht.
+	BEQ Loopback        ; Falls nicht gehe zurück zur Hauptschleife.
+	
+	MOV R1, R0			; Schreibe R0 nach R1 um UART_PutChar aufrufen zu können.
+    MOV R0, R4          ; Setze R0 wieder auf UART_PortNum.
+    BL  UART_PutChar    ; Schreibe das Zeichen erneut.
 
-    END
+	B Loopback
+    ENDP 
+	END
