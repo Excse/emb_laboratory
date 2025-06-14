68abb0f1ffd239219d8353ef73d6216aca54a4af
Timo Behrend <behrendtimo@yahoo.de>
2025-06-14 20:16:04 +0200

    Finished 3-4

--- Lab3CAN_AD/L3-4 Spannung LCD (HW)/CAN_AD_3_4_main.c
+++ Lab3CAN_AD/L3-4 Spannung LCD (HW)/CAN_AD_3_4_main.c
@@ -1,9 +1,8 @@
-
 /*----------------------------------------------------------------------------
- * Name:    CAN_AD_3_4_main.c
- * Purpose: main File for LPC1768 Microcontrollers        
- * Version: Version 1.0 / 02.05.2019 (KBL, HS-Mannheim)
- *----------------------------------------------------------------------------*/
+* Name:    CAN_AD_3_4_main.c
+* Purpose: main File for LPC1768 Microcontrollers        
+* Version: Version 1.0 / 02.05.2019 (KBL, HS-Mannheim)
+*----------------------------------------------------------------------------*/
 
 
 #include "LPC17xx.h"
@@ -11,73 +10,71 @@
 #include "GLCD.h"
 #include <stdio.h>
 
-extern void  ADC_StartConversion(void);
+#define RELOAD_VALUE         249999    // Reload value for 10ms (25MHz * 10ms = 250000) 
 
 void Timer1_Config(void);
 
-
 const uint16_t My_CAN_ID = 0x701;
 
-uint8_t  AD_New_Value_Ready;
-float AD_Voltage;
+extern uint16_t AD_value;
+extern uint8_t AD_New_Value_Ready;
+
+float AD_Voltage = 0.0;
 char NewLine[21], AD_Val_String[6];
 
+int main(void) {
+    GLCD_Init();              /* Initialize the GLCD */
+    GLCD_Clear(White);        /* Clear the GLCD */
+    GLCD_SetBackColor(Blue);  /* Set the Back Color */
+    GLCD_SetTextColor(White); /* Set the Text Color */
 
-int main(void)
-{
-    GLCD_Init();                                    /* Initialize the GLCD */
-    GLCD_Clear(White);                              /* Clear the GLCD */
-  GLCD_SetBackColor(Blue);                        /* Set the Back Color */
-  GLCD_SetTextColor(White);                       /* Set the Text Color */
-    
-    GLCD_DisplayString(0, 0, "  EMB Lab3: CAN/ADC ");/* print string to LCD */
+    GLCD_DisplayString(0, 0, "  EMB Lab3: CAN/ADC ");
     sprintf(NewLine, "   Local ID: %#3x  ", My_CAN_ID);
-    GLCD_DisplayString(1, 0, (unsigned char *) NewLine);/* print string to LCD */
-    GLCD_DisplayString(2, 0, "Local ADValue:");    
-    
+    GLCD_DisplayString(1, 0, (unsigned char *) NewLine);
+    sprintf(NewLine, "Local ADValue: %#.3f", AD_Voltage);
+    GLCD_DisplayString(2, 0, (unsigned char *) NewLine);    
+
     Timer1_Config();
     ADC_Config();
-  ADC_StartConversion();
-    
+    ADC_StartConversion();
+
     AD_New_Value_Ready = 0;
-    
-    while (1) 
-        {
-            if(AD_New_Value_Ready)
-                {
-                    GLCD_SetBackColor(Blue);                        /* Set the Back Color */
-                    GLCD_SetTextColor(White);                       /* Set the Text Color */
-                      /*************************************************/
-                    //Put some code here
-                    //
-                    //
-                    /************************************************/
-                    AD_New_Value_Ready = 0;
-                }
+
+    while (1) {
+        if(AD_New_Value_Ready) {
+            GLCD_SetBackColor(Blue);  /* Set the Back Color */
+            GLCD_SetTextColor(White); /* Set the Text Color */
+            
+            AD_Voltage = ((float) AD_value * 3.3f) / (float) 0xFFF;
+            sprintf(NewLine, "Local ADValue: %#.3f", AD_Voltage);
+            GLCD_DisplayString(2, 0, (unsigned char *) NewLine);    
+            
+            AD_New_Value_Ready = 0;
         }
+    }
 }
 
-void Timer1_Config(void)
-{
-                      /*************************************************/
-                    //Put some code here
-                    //
-                    //
-                    /************************************************/
+void Timer1_Config(void) {
+    LPC_SC->PCONP |= (1 << 2); // Enable Timer1
+
+    // Put some code in order to enable Timer1 interrupt with priority level 1    
+    NVIC_SetPriority(TIMER1_IRQn, 1);
+    NVIC_EnableIRQ(TIMER1_IRQn);
+
+    // and let Timer1 generate an Interrupt every 10ms
+    LPC_TIM1->MR0 = RELOAD_VALUE;        // Zeit[s] * 25MHz = Cycles
+    LPC_TIM1->MCR = (1 << 0) | (1 << 1); // MR0I + MR0R
+    LPC_TIM1->TCR = 1;                   // Timer starten
 }
 
 
-void TIMER1_IRQHandler (void) 
-{  
-    uint16_t AD_Value;
-                          /*************************************************/
-                    //Put some code here
-                    //
-                    //
-                    /************************************************/
-
+void TIMER1_IRQHandler(void) {  
+    // Start new AD conversion
+    ADC_StartConversion();
+    
+    // Clear timer1 interrupt
+    LPC_TIM1->IR = (1 << 0);
 }
-
 
 /************************************ EOF ***********************************/
 
--- Lab3CAN_AD/L3-4 Spannung LCD (HW)/adc.c
+++ Lab3CAN_AD/L3-4 Spannung LCD (HW)/adc.c
@@ -1,24 +1,41 @@
-
 #include "lpc17xx.h"                              /* LPC17xx definitions    */
 #include "adc.h"
+
+#define ADC_MAX_VALUE 0xFFF // Max 12bit Auflösung
+
+uint16_t AD_value = 0;
+uint8_t AD_New_Value_Ready = 0;
 
 /*----------------------------------------------------------------------------
   config ADC
  *----------------------------------------------------------------------------*/
 void ADC_Config (void) {
-
-  //Put your code here to perform the following tasks:
-  //                   1. Enable the AD Converter in the PCONP Register
-  //                2. Configure the AD Pin  -->  AD0.5 Pin
-  //                3. Select AD channel, AD Frequency, enable AD function in the AD module 
+    // 1. Enable the AD Converter in the PCONP Register
+    LPC_SC->PCONP |= (1 << 12);
+    
+    // 2. Configure the AD Pin  -->  AD0.5 Pin
+    LPC_PINCON->PINSEL3 &= ~(3UL << 30);
+    LPC_PINCON->PINSEL3 |=  (3UL << 30);
+    
+    // 3. Select AD channel, AD Frequency, enable AD function in the AD module 
+    LPC_ADC->ADCR = (1 << 5) | (4 << 8) | (1 << 21);
 }
 
 /*----------------------------------------------------------------------------
   start ADC Conversion
  *----------------------------------------------------------------------------*/
 void ADC_StartConversion (void) {
-  //Put code to start an AD conversion                    /* start conversion */
-  //Please remember that a new conversion must start every 100 ms 
+    LPC_ADC->ADCR &= ~(7 << 24); // START-Bits löschen
+    LPC_ADC->ADCR |=  (1 << 24); // START = 001 = Start per Software
+    
+    // Warten bis DONE-Bit gesetzt (bit 31 in ADGDR)
+    while (!(LPC_ADC->ADGDR & (1U << 31)));
+
+    // Wert auslesen: Bits 6 bis 15 enthalten den 12-Bit AD-Wert
+    AD_value = (LPC_ADC->ADGDR >> 4) & ADC_MAX_VALUE;
+     
+      // Flag setzen, dass neue Value ready ist
+      AD_New_Value_Ready = 1;
 }
 
 
